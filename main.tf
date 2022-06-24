7afd219db68b3d193feba8fd38a482af6c67f92b
Jonathan Mikula <jmikula@Jonathans-MacBook-Pro.local>
2022-06-24 16:31:49 -0400

    updated files

--- main.tf
+++ main.tf
@@ -3,10 +3,29 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-03fd5809787d564a0"
-  instance_type = "t2.micro"
+  ami                    = "ami-03fd5809787d564a0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
