25c15bee4fc4b33f4e461ea4eedf4610b66bdff2
jmikula <jmikula@am-jmikula-m67.home>
2022-07-26 00:26:12 -0400

    updated the IAM code for the jsonencode instead of data policy attachment

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -10,25 +10,52 @@ resource "aws_iam_user" "example" {
 resource "aws_iam_policy" "cloudwatch_read_only" {
   description = "This policy provides read only access to Cloudwatch"
   name = "cloudwatch_read_only"
-  policy = "data.aws_iam_policy_document.cloudwatch_read_only.json"
+  #policy = "data.aws_iam_policy_document.cloudwatch_read_only.json"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:Describe*",
+          "cloudwatch:Get*",
+          "cloudwatch:List*"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "neo_cloudwatch_read_only" {
   count = var.give_neo_cloudwatch_full_access ? 0 : 1
   
-  users = aws_iam_user.example[0].name
+  name = "neo_cloudwatch_read_only"
+  users = aws_iam_user.example[each.key].name
   policy_arn = aws_iam_policy.cloudwatch_full_access.arn
 }
 
 resource "aws_iam_policy" "cloudwatch_full_access" {
   description = "This policy provides full access to Cloudwatch"
   name = "cloudwatch_full_access"
-  policy = "data.aws_iam_policy_document.cloudwatch_full_access.json"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:*"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "neo_cloudwatch_full_access" {
   count = var.give_neo_cloudwatch_full_access ? 1 : 0
 
-  users = aws_iam_user.example[0].name
+  name = "neo_cloudwatch_full_access"
+  users = aws_iam_user.example[each.key].name
   policy_arn = aws_iam_policy.cloudwatch_full_access.arn
 }
